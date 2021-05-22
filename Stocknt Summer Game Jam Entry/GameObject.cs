f9d2d174be9fb067ec0dc1cc4dd882a721c90492
CodingSceptile <mukund.arch@gmail.com>
2021-05-21 21:17:11 -0400

    first commit; setup game object

---
+++ Stocknt Summer Game Jam Entry/GameObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Stocknt_Summer_Game_Jam_Entry
+{
+    class GameObject
+    {
+        //Fields
+        protected Rectangle position;
+
+        //Properties
+        /// <summary>
+        /// Property to get or set the rectangle of the game object.
+        /// </summary>
+        public Rectangle Position
+        {
+            get
+            {
+                return position;
+            }
+
+            set
+            {
+                if(value.X > 0 && 
+                   value.Y > 0)
+                {
+                    position = value;
+                }
+            }
+        }
+
+        //Constructor
+        /// <summary>
+        /// Constructor for the GameObject
+        /// </summary>
+        /// <param name="position">Rectangle of the game object.</param>
+        public GameObject(Rectangle position)
+        {
+            this.position = position;
+        }
+    }
+}
---
+++ Stocknt Summer Game Jam Entry/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stocknt_Summer_Game_Jam_Entry
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
