6860856725f7da340b8bc7f27d365eb382211ee6
CodingSceptile <mukund.arch@gmail.com>
2021-05-23 00:13:30 -0400

    starting to load up some textures.

--- Stocknt Summer Game Jam Entry/Game1.cs
+++ Stocknt Summer Game Jam Entry/Game1.cs
@@ -9,6 +9,37 @@ namespace Stocknt_Summer_Game_Jam_Entry
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //My fields
+        //Buttons
+        Button preheat;
+        Button roast;
+        Button stir;
+        Button refrigerate;
+        Button filter;
+
+        //Ingredients
+        Ingredient oliveOil;
+        Ingredient chicken;
+        Ingredient carrots;
+        Ingredient onions;
+        Ingredient celeryWithLeaves;
+        Ingredient dryWhiteWine;
+        Ingredient pepperCorns;
+        Ingredient cloves;
+        Ingredient bayLeaves;
+        Ingredient sprigFreshThyme;
+        Ingredient water;
+
+        //Textures
+        Texture2D buttonTexture;
+        Texture2D oilTexture;
+        Texture2D chickenTexture;
+        Texture2D carrotTexture;
+        Texture2D onionTexture;
+
+        //Misc
+        MouseState mouseState;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -28,6 +59,11 @@ namespace Stocknt_Summer_Game_Jam_Entry
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            oilTexture = Content.Load<Texture2D>("ajay_suresh olive oil");
+            chickenTexture = Content.Load<Texture2D>("scott_rubin chicken");
+            carrotTexture = Content.Load<Texture2D>("carrots");
+            onionTexture = Content.Load<Texture2D>("onions");
+
         }
 
         protected override void Update(GameTime gameTime)
